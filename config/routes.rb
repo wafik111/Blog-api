4bff3a35e4129baf6315c05dcb1611f706609fe3
wafik111 <eng.ahmedwafik@gmail.com>
2020-02-12 12:14:07 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
